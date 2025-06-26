0212a5e3da386b3b6a38b805fac9f7c3b5658ce6
Miller Ren <ren12345@126.com>
2025-06-26 12:26:57 +0800

    upgrade dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import { viteSingleFile } from 'vite-plugin-singlefile'
-import viteCompression from 'vite-plugin-compression'
+import { defineConfig } from "vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
+import viteCompression from "vite-plugin-compression";
 
 // 1.inline assets in index.html
 // 2.compressed it to index.html.gz
@@ -9,39 +9,20 @@ import viteCompression from 'vite-plugin-compression'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    target: 'esnext',
+    target: "es2015",
     assetsInlineLimit: 100000000,
     chunkSizeWarningLimit: 100000000,
     cssCodeSplit: false,
-    brotliSize: false,
-    rollupOptions: {
-      inlineDynamicImports: true,
-      output: {
-        manualChunks: () => 'everything.js'
-      }
-    }
+    brotliSize: false
   },
-  plugins: [viteSingleFile(), viteCompression({
-    deleteOriginFile: true
-  }), viteClean()]
-})
+  plugins: [
+    viteSingleFile({
+      deleteInlinedFiles: true
+    }),
+    viteCompression({
+      deleteOriginFile: true,
+    }),
+    // viteClean(),
+  ],
+});
 
-// clean up unused assets
-function viteClean() {
-  return {
-      name: "vite:clean",
-      transformIndexHtml: {
-          enforce: "post",
-          transform(html, ctx) {
-              // Only use this plugin during build
-              if (!ctx || !ctx.bundle)
-                  return html;
-              // Get the bundle
-              for (const [, value] of Object.entries(ctx.bundle)) {
-                  delete ctx.bundle[value.fileName]
-              }
-              return html
-          },
-      },
-  };
-}
